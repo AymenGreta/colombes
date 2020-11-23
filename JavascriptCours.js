6be2f0c63ea1ed94c13e5c1dd1ecc84d3000e6a6
AymenGreta <lordofaymen@hotmail.fr>
2020-11-23 16:53:27 +0100

    Suite Javascript 1

--- JavascriptCours.js
+++ JavascriptCours.js
@@ -1,3 +1,4 @@
+
 alert ("Salut tout le monde");
 
 // let aymen (variable) = 4 (valeur ); l'expression ici est triviale, elle consiste en le nom d'une autre variable 
@@ -6,10 +7,40 @@ let aymen = 4;
 
 let ayadi = 6;
 
-// let zone échange = aymen; l'expression ici est triviale, elle consiste en le nom d'une autre variable 
+//echange = aymen; l'expression ici est triviale, elle consiste en le nom d'une autre variable 
 
-let zone échange = aymen;
+echange = aymen;
+aymen = ayadi;
 
-let aymen = ayadi;
+// Correction : 
+// Let permet de créer donc variable1 = variable2 permet juste d'échanger, pas besoins d'utiliser de let
 
-let zone échange = ayadi;
+let variable1 = 0;
+let variable2 = 10;
+
+let tampon = variable1;
+
+variable1 = variable2;
+
+variable2 = tampon;
+
+console.log ("La valeur de la 1ère variable + variable1");
+
+console.log ("La valeur de la 2ème variable + variable2");
+
+
+// const message =  c'est un template string 
+
+const nom = "Micka";
+
+const message = `Bonjour ${nom} !`;
+
+console.log (message);
+
+// ça va afficher : Bonjour Micka !
+
+
+// taper sur le navigateur : 
+
+3 && 5
+5
