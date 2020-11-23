6631c2cf2b78a996508e01e6e989afb95ed74466
AymenGreta <lordofaymen@hotmail.fr>
2020-11-23 13:18:03 +0100

    Javascript Jours 1

---
+++ JavascriptCours.js
@@ -0,0 +1,15 @@
+alert ("Salut tout le monde");
+
+// let aymen (variable) = 4 (valeur ); l'expression ici est triviale, elle consiste en le nom d'une autre variable 
+
+let aymen = 4;
+
+let ayadi = 6;
+
+// let zone échange = aymen; l'expression ici est triviale, elle consiste en le nom d'une autre variable 
+
+let zone échange = aymen;
+
+let aymen = ayadi;
+
+let zone échange = ayadi;
