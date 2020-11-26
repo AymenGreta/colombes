b6e39548f382603c5cc0291fdc7bef0cc5b6a0aa
AymenGreta <lordofaymen@hotmail.fr>
2020-11-26 17:12:49 +0100

    exercice boucles

---
+++ Const_T.js
@@ -0,0 +1,24 @@
+const t = ["Hello","chacun","chacune","!"];
+
+for (let i=0; i < t.length; i++) {
+
+console.log(t[i]);  
+   
+}  
+
+
+
+
+
+
+
+console.log("Hello"); 
+
+
+
+
+
+
+
+
+console.log("chacun"); 
--- Exercice_Functions.js
+++ Exercice_Functions.js
@@ -1,14 +1,15 @@
 // Multiplications
-function mult(x,y) { 
-    return x*y
+function mult(x, y) {
+  return x * y;
 }
 
 // Puissances
-function puiss (x=1, y =0) {
- 
-    let resultat = 1;
-    for (let i = 0; i < y ; i++){
-        resultat = mult(resultat, x);
-    }
-    return resultat; 
+function puiss(x = 1, y = 0) {
+  let resultat = 1;
+  for (let i = 0; i < y; i++) {
+    resultat = mult(resultat, x);
+  }
+  return resultat;
 }
+
+const mult = (x, y) => x * y;
